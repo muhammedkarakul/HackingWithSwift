8f90d6121e18d457a2c88bca8f9079b304cbae7f
Muhammed Karakul <muhammedkarakul@gmail.com>
2024-01-06 20:50:29 +0300

    Creates second project

---
+++ SwiftUI/Project-2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mete Karakul on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

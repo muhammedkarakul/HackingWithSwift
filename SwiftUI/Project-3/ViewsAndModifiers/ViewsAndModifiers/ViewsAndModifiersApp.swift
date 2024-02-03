8e578d84b3b232e92048071bf224e01beb0417b3
Muhammed Karakul <muhammedkarakul@gmail.com>
2024-02-03 19:14:37 +0300

    Adds Project-3 folder

---
+++ SwiftUI/Project-3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Mete Karakul on 3.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

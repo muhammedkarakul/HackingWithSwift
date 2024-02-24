6e3f6c45ad62697a10a32a15114e49a135d8b431
muhammed.karakul <muhammed.karakul@innovance.com.tr>
2024-02-25 00:15:56 +0300

    Adds Project-6 Animations

---
+++ SwiftUI/Project-6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Muhammed Karakul on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

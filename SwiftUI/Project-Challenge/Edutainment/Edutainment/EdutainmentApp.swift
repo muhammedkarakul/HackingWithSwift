a9e75a0fbee0e05d58e93edb15316361307afb8e
muhammed.karakul <muhammed.karakul@innovance.com.tr>
2024-04-21 19:20:38 +0300

    Adds Edutainment project

---
+++ SwiftUI/Project-Challenge/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Muhammed Karakul on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

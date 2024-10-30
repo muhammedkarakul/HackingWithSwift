6ee6e0e0b6632a3d4e44fb6f0218f1604303c102
Muhammed Karakul <muhammedkarakul@gmail.com>
2024-10-30 10:28:40 +0300

    Completes day 36 third topic

--- SwiftUI/Project-7/iExpense/iExpense/ContentView.swift
+++ SwiftUI/Project-7/iExpense/iExpense/ContentView.swift
@@ -6,14 +6,22 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class User {
+    var firstName: String = "Bilbo"
+    var lastName: String = "Baggins"
+}
 
 struct ContentView: View {
+    @State var user: User = .init()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
         .padding()
     }
